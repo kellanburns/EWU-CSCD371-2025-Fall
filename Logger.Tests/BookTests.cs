0fa9f89376bb9010fdcf88b3adc73b1e1be98bbf
BillMillerCoding <billmillercoding@outlook.com>
2025-10-20 15:27:01 -0700

    This is some test setup

---
+++ Logger.Tests/BookTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Logger.Tests
+{
+    internal class BookTests
+    {
+    }
+}
---
+++ Logger.Tests/EntityBaseTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Logger.Tests
+{
+    internal class EntityBaseTests
+    {
+        [Fact]
+    }
+}
