b321f518c2a3313904248c419a4397578834ac25
BillMillerCoding <billmillercoding@outlook.com>
2025-10-23 20:02:11 -0700

    Fixed naming comventions

--- Logger/BasePerson.cs
+++ Logger/BasePerson.cs
@@ -1,9 +1,9 @@
 namespace Logger;
 
-public abstract record BasePerson(Guid Id, FullName fullName) : BaseEntity(Id)
+public abstract record BasePerson(Guid Id, FullName FullName) : BaseEntity(Id)
 {
     // Name is implemented implicitly because name should be an obvious functionality of base person
     // Though Id is not being directly implemented in this class, it is using BaseEntities implicit implementation
     public override string Name =>
-        string.IsNullOrWhiteSpace(fullName.ToString()) ? string.Empty : fullName.ToString();
+        string.IsNullOrWhiteSpace(FullName.ToString()) ? string.Empty : FullName.ToString();
 }
--- Logger/Book.cs
+++ Logger/Book.cs
@@ -1,9 +1,9 @@
 namespace Logger;
 
-public record Book(Guid Id, string inputName): BaseEntity(Id)
+public record Book(Guid Id, string InputName): BaseEntity(Id)
 {
     // Name is implemented implicitly because name should be an obvious functionality of Book
     // Though Id is not being directly implemented in this class, it is using BaseEntities implicit implementation
     public override string Name =>
-        string.IsNullOrWhiteSpace(inputName) ? string.Empty : inputName;
+        string.IsNullOrWhiteSpace(InputName) ? string.Empty : InputName;
 }
