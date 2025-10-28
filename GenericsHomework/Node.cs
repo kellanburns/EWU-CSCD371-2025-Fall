c81c6299cb460a66a39fb336d06691855dbdd70a
kellanburns <129882265+kellanburns@users.noreply.github.com>
2025-10-27 21:22:56 -0700

    Minor tweaks to Node.cs

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -1,9 +1,60 @@
-﻿using System;
-
-namespace GenericsHomework;
+﻿namespace GenericsHomework;
 
 public class Node<T>
 {
     private Node<T> _next;
-    public T Value
+    public T Value { get; }
+
+    public Node<T> Next
+    {
+        get => _next;
+        private set => _next = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public Node(T value)
+    {
+        Value = value;
+        _next = this;
+    }
+
+    public override string ToString() => Value?.ToString() ?? string.Empty;
+
+    public Node<T> Append(T value)
+    {
+        if (Exists(value))
+            throw new ArgumentException(nameof(value), "No duplicate values.");
+
+        var newNode = new Node<T>(value);
+        newNode.Next = this.Next;
+        this.Next = newNode;
+
+        return newNode;
+    }
+
+    public void Clear()
+    {
+        // C# garbage collector handles cyclic memory deallocation.
+        // There is no way to access the rest of the list, so they 
+        // will be deallocated without memory leaks.
+        this.Next = this;
+    }
+    
+    public static bool Exists(T value)
+    {
+        var comp = EqualityComparer<T>.Default;
+        var head = this;
+        var cur = head;
+
+        do
+        {
+            if (comp.Equals(cur.Value, value))
+            {
+                return true;
+            }
+
+            cur = cur.Next;
+        } while (!ReferenceEquals(cur, head));
+
+        return false;
+    }
 }
