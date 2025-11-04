37a87e319c3e5f54578a29be06c1e77498d48abb
kellanburns <129882265+kellanburns@users.noreply.github.com>
2025-11-03 20:36:58 -0800

    Generic Calculator.cs finished

--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -1,19 +1,23 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
 
-namespace Calculate
 
-public class Calculator {
-    public static int Add(int a, int b) => a + b;
-    public static int Subtract(int a, int b) => a - b;
-    public static int Multiple(int a, int b) => a * b;
-    public static int Divide(int a, int b)
+namespace Calculate;
+
+public class Calculator <T> where T : INumber<T> {
+    public static T Add(T a, T b) => a + b;
+    public static T Subtract(T a, T b) => a - b;
+    public static T Multiple(T a, T b) => a * b;
+    public static T Divide(T a, T b)
     {
-        if (b == 0) throw new DivideByZeroException();
+        if (b == T.Zero) throw new DivideByZeroException();
         return a / b;
     }
 
-    public IReadOnlyDictionary<char, Func<int, int, int>> MathematicalOperations { get; } =
-        new Dictionary<char, Func<int, int, int>>
+    public IReadOnlyDictionary<char, Func<T, T, T>> MathematicalOperations { get; } =
+        new Dictionary<char, Func<T, T, T>>
         {
             ['+'] = Add,
             ['-'] = Subtract,
@@ -21,8 +25,41 @@ public class Calculator {
             ['/'] = Divide 
         };
         
-    public bool TryCalculate(string calculation, out int result)
+    public bool TryCalculate(string calculation, out T result)
     {
-        
+        result = T.Zero;
+
+        if (string.IsNullOrWhiteSpace(calculation))
+            return false;
+
+        var parts = calculation.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length != 3)
+            return false;
+
+        var leftText = parts[0];
+        var opText = parts[1];
+        var rightText = parts[2];
+
+        if (opText.Length != 1)
+            return false;
+
+        var op = opText[0];
+
+        if (!T.TryParse(leftText, CultureInfo.InvariantCulture, out var left))
+            return false;
+        if (!T.TryParse(rightText, CultureInfo.InvariantCulture, out var right))
+            return false;
+        if (!MathematicalOperations.TryGetValue(op, out var operation))
+            return false;
+
+        try
+        {
+            result = operation(left, right);
+            return true;
+        }
+        catch (DivideByZeroException)
+        {
+            return false;
+        }
     }
 }
