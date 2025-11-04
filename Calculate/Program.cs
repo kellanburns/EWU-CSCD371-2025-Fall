a6cf2bb38f35992856281d5ac536b6ecffb3130c
kellanburns <129882265+kellanburns@users.noreply.github.com>
2025-11-03 20:44:11 -0800

    Program.cs in progress

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Numerics;
+
+namespace Calculate;
+
+public class Program
+{
+    public delegate void WriteLineDelegate(string? text);
+    public delegate string? ReadLineDelegate();
+    public WriteLineDelegate WriteLine { get; init; }
+    public ReadLineDelegate ReadLine { get; init; }
+
+    public Program()
+    {
+        WriteLine = Console.WriteLine;
+        ReadLine = Console.ReadLine;
+    }
+
+    public Program(WriteLineDelegate writeLine, ReadLineDelegate readLine)
+    {
+        WriteLine = writeLine ?? throw new ArgumentNullException(nameof(writeLine));
+        ReadLine = readLine ?? throw new ArgumentNullException(nameof(readLine));
+    }
+    
+    public static int Main()
+    {
+        return 0;
+    }
+}
